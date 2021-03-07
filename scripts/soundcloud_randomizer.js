f976bea2ae62ed7a6152f7de187076329a37d5c6
Allan Farinas <itme@allanfarinas.com>
2021-03-06 22:15:40 -0800

    Initial commit

---
+++ scripts/soundcloud_randomizer.js
@@ -0,0 +1,24 @@
+$(function() {
+  const songIds = [
+    204715554,
+    255320064,
+    303610726,
+    289842554,
+  ];
+  const randomNum = Math.floor(Math.random() * 3);
+
+  const songUrl = 
+    `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${songIds[randomNum]}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true`;
+  $("#soundcloud_iframe").attr('src', songUrl);
+
+  const monitor = setInterval(() => {
+    const elem = document.activeElement;
+    if (elem && elem.tagName == 'IFRAME'){
+      const musicCopy = $(".p-music").text();
+      $(".p-music").text(musicCopy.slice(0, musicCopy.length-1));
+      $("<span style='display: none;'> (🔄 for randomly selected track):</span>").appendTo(".p-music");
+      setTimeout(() => $(".p-music span").fadeIn(3000));
+      clearInterval(monitor);
+    }
+  }, 100);
+});
---
+++ scripts/typing_animation.js
@@ -0,0 +1,48 @@
+$(function() {
+  const animateTyping = async (el, nextCharDelay, beginDelay, endDelay) => {
+    return new Promise((resolve) => {
+      const textArray = $(el).text().split("");
+      $(el).html("");
+      $("<span class='cursor' />").appendTo(el);
+
+      textArray.forEach(function(c, index) {
+        setTimeout(function() {
+          const currentText = $(el).text();
+          $(el).text(currentText + c);
+          $("<span class='cursor' />").appendTo(el);
+        }, beginDelay + (nextCharDelay * (index + 1)));
+      });
+
+      const totalAnimationTime = (textArray.length * nextCharDelay) + endDelay;
+      setTimeout(() => {
+        $(".cursor").hide();
+        resolve(true);
+      }, totalAnimationTime);
+    });
+  };
+
+  const animateElements = async () => {
+    return new Promise(async (resolve) => {
+      const nextCharDelay = 100;
+      const beginDelay = 2000;
+      const endDelay = 4000;
+      const elementsToAnimate = $(".typing-animation").toArray();
+      
+      $(".typing-animation").css("visibility", "hidden");
+      const totalAnimationTime = elementsToAnimate.reduce((acc, curr) =>  acc + ($(curr).text().length * nextCharDelay) + beginDelay + endDelay);
+  
+      for (const el of elementsToAnimate) {
+        $(el).css("visibility", "visible");
+        await animateTyping(el, nextCharDelay, beginDelay, endDelay);
+      }
+
+      setTimeout(() => {
+        resolve(true);
+        
+      }, totalAnimationTime);
+    });
+  };
+
+  setTimeout(() => $("#soundcloudIframeContainer").hide(), 2000);
+  animateElements().then(() => $("#soundcloudIframeContainer").fadeIn(1500));
+});
